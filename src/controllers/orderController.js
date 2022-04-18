bf90e06980ee65e65108091014cd044db9a33696
dimple jha <dimplejha98@gmail.com>
2022-04-19 00:31:11 +0530

    completed

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -3,131 +3,84 @@ const userModel = require("../model/userModel");
 const cartModel = require("../model/cartModel");
 const orderModel = require("../model/orderModel");
 
-//Creating order
-const createOrder = async function (req, res) {
-    try {
-        
-        req.body.userId = req.params.userId
-        let totalQuantity = 0;
 
-        if(!validator.isValidRequestBody(req.body)){
-            return res.status(400).send({status:false, message:"Bad Request request body is empty"})
-        }
-        const {items, totalPrice,  totalItems} = req.body
-
-        const validItems =  items.filter((obj) => {
-            return obj != null && Object.keys(obj).length
-        })
-    
-        if(!(items.length && validItems.length)){
-            return res.status(400).send({status:false, message:"please select the product to place order"})
-        }
-        
-        if(!(validator.isValidPrice(totalPrice) && validator.isValid(totalPrice))){
-            return res.status(400).send({status:false, message:"pease provide price or enter valid price"})
-        }
-
-        if(!(/^[1-9]{1}[0-9]{0,15}$/.test(totalItems) && validator.isValid(totalItems))){ 
-
-            return res.status(400).send({status:false, message:"Bad request please provide valid totalItems"})
-        
-        }
-
-
-            items.forEach((productObj) => {
-                    totalQuantity += Number(productObj.quantity)
-                })
-        req.body.totalQuantity = totalQuantity
-
-
-
-        
-
-           const orderData = await orderModel.create(req.body)
-             return res.status(201).send({ status: true, msg: "Order created succesfully", data: orderData })
-
-
-    } catch (error) {
-        return res.status(500).send({ status: false, error: error.message })
-    }
-}
 
 
 
 //---------------------------------1st--------------------------------------------------------------------------------------------------------------------
-// let createOrder = async function (req, res) {
-//     try {
-//         const userId = req.params.userId;
-//         const idFromToken = req.userId
-//         if (!validator.isValid(userId)) {
-//             return res.status(400).send({ status: false, message: "Enter the userId" });
-//         }
-//         if (!validator.isValidObjectId(userId)) {
-//             return res.status(400).send({ status: false, message: "Enter a valid userId" });
-//         }
+let createOrder = async function (req, res) {
+    try {
+        const userId = req.params.userId;
+        const idFromToken = req.userId
+        if (!validator.isValid(userId)) {
+            return res.status(400).send({ status: false, message: "Enter the userId" });
+        }
+        if (!validator.isValidObjectId(userId)) {
+            return res.status(400).send({ status: false, message: "Enter a valid userId" });
+        }
 
-//         const user = await userModel.findOne({ _id: userId });
-//         if (!user) {
-//             return res.status(404).send({ status: false, message: "User not found" });
-//         }
+        const user = await userModel.findOne({ _id: userId });
+        if (!user) {
+            return res.status(404).send({ status: false, message: "User not found" });
+        }
 
-//         if (userId != idFromToken) {
-//             return res.status(403).send({ status: false, message: "User not authorized" })
-//         }
+        if (userId != idFromToken) {
+            return res.status(403).send({ status: false, message: "User not authorized" })
+        }
 
-//         const requestBody = req.body;
+        const requestBody = req.body;
 
-//         if (!validator.isValidRequestBody(requestBody)) {
-//             return res.status(400).send({ status: false, message: "Enter cart details" });
-//         }
+        if (!validator.isValidRequestBody(requestBody)) {
+            return res.status(400).send({ status: false, message: "Enter cart details" });
+        }
 
-//         const { cartId } = requestBody;
-//         if (!validator.isValid(cartId)) {
-//             return res.status(400).send({ status: false, message: "Enter the cartId" });
-//         }
+        const { cartId } = requestBody;
+        if (!validator.isValid(cartId)) {
+            return res.status(400).send({ status: false, message: "Enter the cartId" });
+        }
 
-//         if (!validator.isValidObjectId(cartId)) {
-//             return res.status(400).send({ status: false, message: "Enter a valid cartId" });
-//         }
-//         const cartAlreadyPresent = await cartModel.findOne({ _id: cartId });
-//         if (!cartAlreadyPresent) {
-//             return res.status(404).send({ status: false, message: "cart not found" });
-//         }
+        if (!validator.isValidObjectId(cartId)) {
+            return res.status(400).send({ status: false, message: "Enter a valid cartId" });
+        }
+        const cartAlreadyPresent = await cartModel.findOne({ _id: cartId });
+        if (!cartAlreadyPresent) {
+            return res.status(404).send({ status: false, message: "cart not found" });
+        }
 
-//         if (cartAlreadyPresent.userId != userId) {
-//             return res.status(400).send({ status: false, message: "With this user cart is not created" });
-//         }
+        if (cartAlreadyPresent.userId != userId) {
+            return res.status(400).send({ status: false, message: "With this user cart is not created" });
+        }
 
-//         if (cartAlreadyPresent.totalItems == 0) {
-//             return res.status(400).send({ status: "SUCCESS", message: "There is no product to order ,First add product" })
-//         }
+        if (cartAlreadyPresent.totalItems == 0) {
+            return res.status(400).send({ status: "SUCCESS", message: "There is no product to order ,First add product" })
+        }
 
-//         let totalPrice = cartAlreadyPresent.totalPrice;
-//         let totalItems = cartAlreadyPresent.items.length
-//         let totalQuantity = 0
+        let totalPrice = cartAlreadyPresent.totalPrice;
+        let totalItems = cartAlreadyPresent.items.length
+        let totalQuantity = 0
 
-//         let itemsArr = cartAlreadyPresent.items
-//         for (i in itemsArr) {
-//             totalQuantity += itemsArr[i].quantity
+        let itemsArr = cartAlreadyPresent.items
+        for (i in itemsArr) {
+            totalQuantity += itemsArr[i].quantity
 
-//         }
+        }
 
-//         let newOrder = {
-//             userId: userId,
-//             items: cartAlreadyPresent.items,
-//             totalPrice: totalPrice,
-//             totalItems: totalItems,
-//             totalQuantity: totalQuantity
-//         };
+        let newOrder = {
+            userId: userId,
+            items: cartAlreadyPresent.items,
+            totalPrice: totalPrice,
+            totalItems: totalItems,
+            totalQuantity: totalQuantity
+        };
 
-//         orderData = await orderModel.create(newOrder);
-//         return res.status(200).send({ status: "SUCCESS", message: "Order placed successfully", data: orderData });
+        orderData = await orderModel.create(newOrder);
+        return res.status(200).send({ status: "SUCCESS", message: "Order placed successfully", data: orderData });
 
-//     }
-//     catch (error) {
-//         return res.status(500).send({ status: false, message: error.message })
-//     }
-// }
+    }
+    catch (error) {
+        return res.status(500).send({ status: false, message: error.message })
+    }
+}
 
 //----------------------------------------------------------2api --------------------------------------------------------------------
 
@@ -181,6 +134,39 @@ const updateOrder = async function (req, res) {
         return res.status(500).send({ status: false, msg: err.message })
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
